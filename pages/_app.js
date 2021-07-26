771ac1a0adc0526b22fa651f466a98410c03a76c
jpuri <jyotipuri@gmail.com>
2021-07-26 19:25:00 +0530

    Implementing very basic integration to florencenet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script async src="https://www.tezbridge.com/plugin.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import { TezosToolkit } from "@taquito/taquito";
+import { TezBridgeSigner } from "@taquito/tezbridge-signer";
+
 export default function Home() {
+  if (typeof window !== "undefined") {
+    const tezos = new TezosToolkit("https://api.tez.ie/rpc/florencenet");
+    tezos.setProvider({ signer: new TezBridgeSigner() });
+    tezos.tz
+      .getBalance("tz1h3rQ8wBxFd8L9B3d7Jhaawu6Z568XU3xY")
+      .then((balance) => console.log(`${balance.toNumber() / 1000000} ꜩ`))
+      .catch((error) => console.log(JSON.stringify(error)));
+  }
   return <div>Frontend implementation for Recollect DAO.</div>;
 }
